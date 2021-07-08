a381d023a3a5470cf45711da4ebb3336848629e0
귀염뽀짝유경이 <65803951+PrettyYukyoung@users.noreply.github.com>
2021-07-08 23:46:18 +0900

    programming

---
+++ CodeUp/1402번_거꾸로_출력하기-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    int n;
+    int a[1002]={0,};
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+    	scanf("%d",&a[i]);
+	}
+    for(int i=n-1;i>=0;i--){
+    	printf("%d ",a[i]);
+	}
+}
